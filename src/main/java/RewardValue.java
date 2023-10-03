6396ffda6d0a2cecae0f4e5df54ac6c079e7767c
atiwhocodes <atiwhocodes@gmail.com>
2023-10-03 23:29:23 +0100

    Implemented RewardValue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,21 @@
+// RewardValue.java
+
+public class RewardValue {
+    // Member variable
+    private double value;
+
+    // Constructor
+    public RewardValue(double value) {
+        this.value = value;
+    }
+
+    // Method to get the reward value
+    public double getValue() {
+        return value;
+    }
+
+    // Method to set the reward value
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
